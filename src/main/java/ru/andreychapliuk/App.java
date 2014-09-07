26cd4a83c4252c3f1a4a147f001446d4667f5580
Amagister <magister@list.ru>
2014-09-07 20:17:12 +0300

    init commit

---
+++ src/main/java/ru/andreychapliuk/App.java
@@ -0,0 +1,19 @@
+package ru.andreychapliuk;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("enter num:");
+    	int num = scanner.nextInt();
+    	System.out.println(num);
+        System.out.println( "Hello World!" );
+    }
+}
